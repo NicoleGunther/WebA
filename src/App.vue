da650ae0db6e7cb3b67baa7435501b8e0161b181
NicoleGunther <nicoleguntherg@hotmail.com>
2022-08-25 17:56:05 -0500

    correction

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+ <div class="container p-5">
+ <h1 class="text-center border-bottom pb-3 mb-3">Vue 3 Showing PostgreSQL Data Using NodeJS Express WEB API V2</h1>
+  <table class="table table-hover table-bordered mt-5">
+    <thead>
+      <tr>
+        <th scope="col">Datetime</th>
+        <th scope="col">w3</th>
+        <th scope="col">u3</th>
+      </tr>
+    </thead>
+    <tbody>
+     <tr v-for="m in meas" :key="m.Datetime">
+        <td>{{m.Datetime}}</td>
+        <td>{{m.w3}}</td>
+        <td>{{m.u3}}</td>
+      </tr>    
+    </tbody>
+  </table>
+  
+ 
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+<script>
+window.alert(5 + 6);
+//importing bootstrap 5 and axios
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from 'axios'
+export default {
+  data(){
+  return {
+    meas:[]
+    
+    }
+  },
+ mounted () {
+    //api caling
+     axios
+      .get('http://150.136.7.153:5000/meas')
+      .then((resp) => {
+        window.alert(resp);
+        this.meas = resp.data;
+      })
+  },
+ 
 }
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
+</script>
